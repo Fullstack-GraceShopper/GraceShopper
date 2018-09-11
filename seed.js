1f9af04bd7b82784431914aef96f7c36750b5f27
James Byrd <James.Byrd1265@gmail.com>
2018-09-11 19:54:36 -0400

    Initial API route to /socks created, but db still not seeding

--- seed.js
+++ seed.js
@@ -5,28 +5,25 @@ const db = require('./server/db');
 const {Sock} = require('./server/db/models')
 
 const seed = async (num) => {
-  await db.sync({force: true})
+ await db.sync({force: true})
 
-  for(let i = 0; i < num; i++){
-    await Sock.create({
-      photos: [faker.fake("{{image.technics}}")],
-      name: faker.fake("{{commerce.productName}}"),
-      price: faker.fake("{{commerce.price}}"),
-      isAdult: true,
-      sizes: [12, 6, 9, 4],
-      category: [faker.fake("{{commerce.product}}")]})
-  }
+ for(let i = 0; i < num; i++){
+   await Sock.create({
+     photos: [faker.fake("{{image.technics}}")],
+     name: faker.fake("{{commerce.productName}}"),
+     price: faker.fake("{{commerce.price}}"),
+     isAdult: true,
+     sizes: [12, 6, 9, 4],
+     category: [faker.fake("{{commerce.product}}")]})
+ }
 
-  console.log(bgMagenta('Seeding success!'))
-  db.close()
+ console.log(bgMagenta('Seeding success!'))
+ db.close()
 }
 
 seed(10)
-  .catch(err => {
-    console.error(red('Oh noes! Something went wrong!'))
-    console.error(err)
-    db.close()
-  })
-
-
-
+ .catch(err => {
+   console.error(red('Oh noes! Something went wrong!'))
+   console.error(err)
+   db.close()
+ })
