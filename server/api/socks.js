f74b336460322d35c6d38735e0ecabcbf9e3457d
Laura R <laurarachel@gmail.com>
2018-09-12 12:32:30 -0700

    reverted to before api split

---
+++ server/api/socks.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const { Sock } = require('../db/models')
+
+router.get('/adults', async (req, res, next) => {
+  try {
+    const socks = await Sock.findAll({where: {isAdult: true}})
+    res.json(socks)
+  } catch(err) {
+    next(err)
+  }
+})
+
+router.get('/kids', async (req, res, next) => {
+  try {
+    const socks = await Sock.findAll({where: {isAdult: false}})
+    res.json(socks)
+  } catch(err) {
+    next(err)
+  }
+})
+
+router.get('/:sockId', async(req, res, next) => {
+  try {
+    const sock = await Sock.findOne(req.params.sockId)
+    res.json(sock)
+  } catch(err) {
+    next(err)
+  }
+})
+
+module.exports = router
