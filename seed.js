37f4a2ce5cf85d2934c9a4bcfada2ed730a222aa
Morgan Brown <Morganb816@gmail.com>
2018-09-11 20:01:16 -0400

    added users to seed file aswell as some helper functions for data not supported by faker.js

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 
 const {bgMagenta, red} = require('chalk');
 const db = require('./server/db');
-const {Sock} = require('./server/db/models')
+const {Sock, User} = require('./server/db/models')
 
 const seed = async (num) => {
   await db.sync({force: true})
@@ -12,16 +12,46 @@ const seed = async (num) => {
       photos: [faker.fake("{{image.technics}}")],
       name: faker.fake("{{commerce.productName}}"),
       price: faker.fake("{{commerce.price}}"),
-      isAdult: true,
-      sizes: [12, 6, 9, 4],
-      category: [faker.fake("{{commerce.product}}")]})
+      isAdult: boolGen([3,10]),
+      sizes: arrGen([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], 5),
+      category: [faker.fake("{{commerce.product}}")]});
+
+    await User.create({
+      email:  faker.fake("{{internet.email}}"),
+      password: faker.fake("{{internet.password}}"),
+      photo: faker.fake("{{image.avatar}}"),
+      address: faker.fake("{{address.state}}, {{address.zipCode}}, {{address.streetAddress}}"),
+    });
   }
 
   console.log(bgMagenta('Seeding success!'))
   db.close()
 }
 
-seed(10)
+// ==> MISC RANDOM GENERATORS <== //
+
+const boolGen = (odds) => {
+  // odds is an array... ex ==> [3,10] = 3/10
+  if (randomGenerator(odds[1]) > odds[0]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const arrGen = (options, maxOptions) => {
+  let output = [];
+  for (let i = 0; i < randomGenerator(maxOptions); i ++) {
+    output.push(options[randomGenerator(options.length)])
+  };
+  return output
+}
+
+const randomGenerator = (max) => {
+  return Math.floor(Math.random() * Math.floor(max - 1)) + 1;
+}
+
+seed(50)
   .catch(err => {
     console.error(red('Oh noes! Something went wrong!'))
     console.error(err)
