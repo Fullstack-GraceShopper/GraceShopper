d3de4436925b804dbf2d38da8b9be50a5fd9e3ca
Laura R <laurarachel@gmail.com>
2018-09-13 21:35:48 -0700

    style: made adult and kids views match wireframe, color still questionable for header, updates kids view to also link to single sock view

--- client/components/kid-socks.js
+++ client/components/kid-socks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchKidSocks } from '../store/socks'
+import {Link} from 'react-router-dom'
 
 class KidSocks extends React.Component {
   async componentDidMount () {
@@ -11,20 +12,20 @@ class KidSocks extends React.Component {
     const { kidSocks } = this.props
 
     return (
-      <div>
-        <div>
+      <div className="flex column center container-space-around">
+        <div className="flex center category-header">
           <h1>Kids Sock</h1>
         </div>
-        <br />
-        <br />
 
         <div>
           { kidSocks.length > 0
-          ? <div>
+          ? <div className="flex row wrap">
               { kidSocks.map((sock) => { return (
-                <div key={sock.id}>
-                  <img src={sock.photos[0]} />
-                </div>)
+                <Link key={sock.id} to={`/socks/${sock.id}`}>
+                  <img className="sock-image" src={sock.photos[0]} />
+                  <div>{sock.name}</div>
+                  <div>{sock.price}</div>
+              </Link>)
               })}
             </div>
           : <div> There are no sock registered to the database </div>
