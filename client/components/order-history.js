76c447c0d89ee366050a13b1c1dd7bd448eb9d37
James Byrd <James.Byrd1265@gmail.com>
2018-09-15 21:41:10 -0400

    feat: order-history renders dates

--- client/components/order-history.js
+++ client/components/order-history.js
@@ -3,13 +3,19 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {fetchOrderHistory} from '../store/orders'
 import axios from 'axios';
+const formatDate = order => {
+  let date = order.createdAt.slice(0,10).split('-')
+  console.log('DATE:  ', date)
+  const year = date.shift()
+  date.push(year)
+  return date.join('-')
+}
 
 class OrderHistory extends Component {
 
   async componentDidMount () {
     
     try {
-      console.log(this.props.match.params)
       const userId = Number(this.props.match.params.userId)
       await this.props.getOrderHistory(userId)
     } catch(err) {
@@ -26,9 +32,11 @@ class OrderHistory extends Component {
           {orderHistory.map(order => {
             return (
               <div key={order.id}>
+                <br/>
                 <Link key={order.id} 
                   to={`orders/${order.userId}/order-history`}
                 >{order.id}</Link>
+                <h5>{formatDate(order)}</h5>
               </div>
             )
           })}
