b5e141fedc6fcb40ffd664ee09e98774100fa33a
Dan Gilbert <daniel.gilbert0051@gmail.com>
2018-09-18 11:28:18 -0400

    bugfix: header will no longer not update if you do not click directly on category name

--- client/components/category-menu.js
+++ client/components/category-menu.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React from 'react'
 import {Link} from 'react-router-dom'
 import {categoryArr} from './utils'
 console.log('cat:   ', categoryArr)
 
-const CategoryMenu = (props) => {
+const CategoryMenu = props => {
   return (
     <div>
       <div id="categorys-menu">
-      {categoryArr.map(category => {
-        let name = category.name
-        return (
-          <div onClick={() => props.onClick(`${name}`)} className="category-menu-item hover-light" key={name}>
-            <Link className="no-decoration white" to={`/socks/category/${name}`}>
+        {categoryArr.map(category => {
+          let name = category.name
+          return (
+            <Link
+              key={name}
+              className="no-decoration white"
+              to={`/socks/category/${name}`}
+            >
+              <div
+                onClick={() => props.onClick(`${name}`)}
+                className="category-menu-item hover-light"
+                key={name}
+              >
                 {name}
+              </div>
             </Link>
-          </div>
-        )  
-      })}
+          )
+        })}
       </div>
     </div>
   )
