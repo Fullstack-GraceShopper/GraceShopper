54b17e6db1d555d7f4595d6bacf2771d1fe63f07
jbprojectlab <james.byrd1265@gmail.com>
2019-07-28 12:20:36 -0400

    feat: empty order history provides link to start shopping

--- client/components/order-history.js
+++ client/components/order-history.js
@@ -1,50 +1,47 @@
-  import React, {Component} from 'react'
+import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getOrders} from '../store/orders'
 import {formatDate} from './utils'
 
-class OrderHistory extends Component {
-  async componentDidMount () {
-    try {
-      const userId = Number(this.props.user.id)
-      await this.props.getOrderHistory(userId)
-    } catch(err) {
-      console.error(err)
-    }
-  }
+const OrderHistory = ({user, orders, getOrderHistory}) => {
 
-  render() {
-    const orderHistory = this.props.orders
-    return(
-      !orderHistory
-      ? <h1>no orders</h1>
-      : <table className="order-history">
-          <thead>
-          </thead>
-          <tbody>
-            <tr>
-              <td>ID</td>
-              <td>Date</td>
-            </tr>   
-              {orderHistory.map(order => {
-                return (
-                  <tr className="orderRow" key={order.id}>
-                    <td>
-                      <Link to={`/${order.userId}/orders/${order.id}`} key={order.id}>
-                        {order.id}
-                      </Link>
-                    </td>
-                    <td>
-                      <h5>{formatDate(order)}</h5>
-                    </td>
-                  </tr>
-                )
-              })}
-          </tbody>
-        </table> 
-    )
-  } 
+  const userId = Number(user.id)
+
+  useEffect(() => {
+    getOrderHistory(userId)
+  }, [getOrderHistory])
+
+  return !orders.length ? (
+    <div>
+      <h4>You haven't placed any orders yet</h4>
+      <Link to='/'>Start Shopping</Link>
+    </div>
+    ):(
+    <table className="order-history">
+      <thead>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ID</td>
+          <td>Date</td>
+        </tr>   
+        {orders.map(order => (
+            <tr className="orderRow" key={order.id}>
+              <td>
+                <Link to={`/${order.userId}/orders/${order.id}`} key={order.id}>
+                  {order.id}
+                </Link>
+              </td>
+              <td>
+                <h5>{formatDate(order)}</h5>
+              </td>
+            </tr>
+          )
+        )}
+      </tbody>
+    </table> 
+  )
 }
 
 const mapStateToProps = ({orders}) => ({orders})
