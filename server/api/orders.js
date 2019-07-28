7878510ca497df475d81cd0f986822f1f18fa59e
jbprojectlab <james.byrd1265@gmail.com>
2019-07-27 23:04:58 -0400

    fix: order history route restored

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,23 +1,21 @@
 const router = require('express').Router()
 const {Sock, Order, CartItem, User} = require('../db/models')
 
-// //new route for getting order history using query
-// router.get('/', async (req, res, next) => {
-//     let orders = []
-//     try {
-//       if(Number(req.query.userId) === req.user.id) {
-//         orders = await Order.findAll({
-//         where: {
-//           userId: req.user.id,
-//           sold: true
-//         }
-//       })
-//       res.json(orders)
-//     } else res.sendStatus(401)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+router.get('/', async (req, res, next) => {
+    let orders = []
+    try {
+      if(Number(req.query.userId) === req.user.id) {
+        orders = await Order.findAll({
+        where: {
+          userId: req.user.id
+        }
+      })
+      res.json(orders)
+    } else res.sendStatus(401)
+    } catch (err) {
+        next(err)
+    }
+})
 
 router.post('/', async (req, res, next) => {
   const {id, email} = req.body
