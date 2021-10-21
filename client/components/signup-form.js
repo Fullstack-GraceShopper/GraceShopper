333d9075006ffa24f2813d12e9223662e0c8dc6e
jbprojectlab <james.byrd1265@gmail.com>
2021-10-21 15:24:31 -0400

    refactor: create shipping-info component

--- client/components/signup-form.js
+++ client/components/signup-form.js
@@ -3,38 +3,28 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {signUp} from '../store'
 
-const SignUp = props => {
-  const {name, displayName, handleSubmit, error} = props
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <label htmlFor="address">
-            <small>Address</small>
-          </label>
-          <input name="address" type="text" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
-  )
-}
+const SignUp = ({name, displayName, handleSubmit, error}) => (
+  <div>
+    <form onSubmit={handleSubmit} name={name}>
+      <div>
+        <label htmlFor="email">
+          <small>Email</small>
+        </label>
+        <input name="email" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">
+          <small>Password</small>
+        </label>
+        <input name="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{displayName}</button>
+      </div>
+      {error && error.response && <div> {error.response.data} </div>}
+    </form>
+  </div>
+)
 
 const mapLogin = state => {
   return {
@@ -55,11 +45,11 @@ const mapSignup = state => {
 const mapDispatch = dispatch => {
   return {
     handleSubmit(evt) {
+      console.log('event:  ', evt.target)
       evt.preventDefault()
       const email = evt.target.email.value
-      const address = evt.target.address.value
       const password = evt.target.password.value
-      dispatch(signUp(email, password, address))
+      dispatch(signUp(email, password))
     }
   }
 }
